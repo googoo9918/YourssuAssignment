bcd761ac1abbff72108b8541b219571fd4aca2b2
jungdonggyu <1013as@naver.com>
2023-10-10 19:22:58 +0900

    Feat: "회원가입 request 및 response 수정"

--- src/main/kotlin/yourssu/yourssuAssigmnet/domain/user/dto/UserDto.kt
+++ src/main/kotlin/yourssu/yourssuAssigmnet/domain/user/dto/UserDto.kt
@@ -3,6 +3,7 @@ package yourssu.yourssuAssigmnet.domain.user.dto
 import lombok.AllArgsConstructor
 import lombok.Getter
 import yourssu.yourssuAssigmnet.domain.common.dto.BaseUserDto
+import yourssu.yourssuAssigmnet.domain.user.constant.Role
 import javax.validation.constraints.NotBlank
 
 class UserDto {
@@ -10,13 +11,15 @@ class UserDto {
     @Getter
     data class Post(
         @field:NotBlank(message = "이름을 입력해주세요")
-        val username: String
+        val username: String,
+        val role: Role
     ) : BaseUserDto()
 
     @Getter
     @AllArgsConstructor
     data class Response(
         val email: String,
-        val username: String
+        val username: String,
+        val role: Role
     )
 }
